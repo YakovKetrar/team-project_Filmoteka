666d6dd5d357cc8be5e2ff42c9ec8b3b5f50f255
Igor Harchenko <1305hariok@ukr.net>
2021-09-04 15:55:05 +0300

    Создает файлик localStorage

---
+++ src/js/localStorage.js
@@ -0,0 +1,8 @@
+// import { refs } from './refs';
+
+// const onWatchedBtn = document.querySelector('#watchedBtn');
+// const onQueueBtn = document.querySelector('#queueBtn');
+
+// onWatchedBtn.addEventListener('click',);
+// onQueueBtn.addEventListener('click',);
+
